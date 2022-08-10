b6a48b9eb5108c7f3fb3e3fa19b378ffdc0cbe49
Vaibhav T Gaikwad <103184302+VaibhavGaikwad03@users.noreply.github.com>
2022-08-10 12:23:33 +0530

    Add files via upload

---
+++ Read_Data.c
@@ -0,0 +1,41 @@
+//Write application which accept file name from user and read all the data from that file and display contents on screen.
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<unistd.h> 
+#include<fcntl.h>
+#include<string.h>
+
+int main()
+{
+    char FileName[20];     
+    char Data[100]; 
+    int fd = 0;     //File descripter
+    int iRet = 0;
+
+    printf("Enter file name to open : \n");
+    scanf("%s", FileName);
+
+    fd = open(FileName, O_RDONLY | O_APPEND);
+
+    if(fd == -1)
+    {
+        printf("Unable to open the file\n");
+        return -1;
+    }
+    printf("File opened succesfully with fd : %d\n", fd);
+
+    iRet = read(fd, Data, 10);
+
+    printf("%d bytes get succesfully read from file\n", iRet);
+
+    printf("Data from file is : \n");
+
+    write(1, Data, iRet);
+
+    //0 STDIN : Keyboard
+    //1 STDOUT : Console
+    //2 STDERR : For Error
+
+    return 0;
+}

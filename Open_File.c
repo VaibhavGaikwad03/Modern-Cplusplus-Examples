6c5e77c749b801657160f1daab60f1e08477d621
Vaibhav T Gaikwad <103184302+VaibhavGaikwad03@users.noreply.github.com>
2022-08-10 11:45:16 +0530

    Add files via upload

--- Open_File.c
+++ Open_File.c
@@ -2,19 +2,19 @@
 
 #include<stdio.h>
 #include<stdlib.h>
-#include<unistd.h>
+#include<unistd.h>      //Header file for use the open function
 #include<fcntl.h>
 #include<string.h>
 
 int main()
 {
-    char FileName[20];
+    char FileName[20];      
     int fd = 0;
 
     printf("Enter file name to open : \n");
     scanf("%s", FileName);
 
-    fd = open(FileName, O_RDONLY);
+    fd = open(FileName, O_RDONLY);  //Function to open the file
 
     if(fd == -1)
     {
